d7464e11322c08137e25b3e7353d03d8860ae7f4
Ahmed-Ezzeldin <ahmed.ezzeldin.ibrahim@gmail.com>
2024-01-10 21:46:09 +0200

    Removing Records

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Query,
+  Delete,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -24,8 +25,13 @@ export class UsersController {
     return this.userService.findOne(parseInt(id));
   }
 
-  @Get('')
+  @Get()
   findAllUsers(@Query('email') email: string) {
     return this.userService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id));
   }
 }
