96a5b16639c3be29d5d123d64425105baf76d0fd
Ahmed-Ezzeldin <ahmed.ezzeldin.ibrahim@gmail.com>
2024-01-10 21:56:54 +0200

    Updating Records

---
+++ src/users/dtos/update-user-dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsOptional } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsEmail()
+  @IsOptional()
+  email: string;
+
+  @IsOptional()
+  @IsOptional()
+  password: string;
+}
