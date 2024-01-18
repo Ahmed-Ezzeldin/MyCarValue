0641b5e704ef0146aff9f4d4374b1d8e25936701
Ahmed-Ezzeldin <ahmed.ezzeldin.ibrahim@gmail.com>
2024-01-18 22:09:33 +0200

    Implementing Signup Functionality

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+
+  signup(email: string, password: string) {
+    // See if email already exists
+    const users = this.usersService.find(email);
+    if (users) {
+      throw new BadRequestException('Email already exists');
+    }
+    // Hash the users passsword
+    // Create a new user and save it
+    // Return the new user
+  }
+
+  signin() {}
 }
