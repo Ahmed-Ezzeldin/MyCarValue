36f14d55b2fd46f7c5640d28696970ad26c0894f
Ahmed-Ezzeldin <ahmed.ezzeldin.ibrahim@gmail.com>
2024-01-20 22:37:23 +0200

    Getting the Current User

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -28,6 +28,11 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
+  @Get('/whoami')
+  whoAmI(@Session() session: any) {
+    return this.userService.findOne(session.userId);
+  }
+
   @Get('/colors/:color')
   setColor(@Param('color') color: string, @Session() session: any) {
     session.color = color;
